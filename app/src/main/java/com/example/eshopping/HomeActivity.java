56c67b70741bb8126a99758bec55f8500105da89
NarendraUnnava <89415190+NarendraUnnava@users.noreply.github.com>
2022-03-21 14:35:46 -0500

    Create HomeActivity.java

---
+++ app/src/main/java/com/example/eshopping/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.eshopping;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    HomeAdapter homeAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        recyclerView= findViewById(R.id.rv);
+        homeAdapter = new HomeAdapter();
+        LinearLayoutManager manager = new LinearLayoutManager(HomeActivity.this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(homeAdapter);
+
+    }
+}

12fc81b925aa8f68fc3a42b78136970e5db74ac6
NarendraUnnava <89415190+NarendraUnnava@users.noreply.github.com>
2022-03-21 14:35:50 -0500

    Create HomeAdapter.java

---
+++ app/src/main/java/com/example/eshopping/HomeAdapter.java
@@ -0,0 +1,35 @@
+package com.example.eshopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public HomeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_home, parent, false);
+
+        return new HomeAdapter.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}

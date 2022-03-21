cc348c13ffe03e18979ee615f45cb620f479e97e
akhilyarlagadda <S545513@nwmissouri.edu>
2022-03-21 14:56:42 -0500

    Created Splash Activity

---
+++ app/src/main/java/com/example/eshopping/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.example.eshopping;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private Context mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        splash();
+
+    }
+
+    private void splash() {
+        final boolean _active = true;
+        final int _splashTime = 1000;
+
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (_active && (waited < _splashTime)) {
+
+                        sleep(100);
+                        if (_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+
+                    Intent intent = new Intent(mContext, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        splashTread.start();
+    }
+
+}

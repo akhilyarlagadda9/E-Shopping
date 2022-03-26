8d09699e8bd916d91e78729f422e62a1734f4334
akhilyarlagadda <S545513@nwmissouri.edu>
2022-03-25 23:53:53 -0500

    Create Fruits activity - Akhil Babu Yarlagadda

---
+++ app/src/main/java/com/example/eshopping/FruitsModel.java
@@ -0,0 +1,24 @@
+package com.example.eshopping;
+
+public class FruitsModel {
+    int icon;
+    String title;
+
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}

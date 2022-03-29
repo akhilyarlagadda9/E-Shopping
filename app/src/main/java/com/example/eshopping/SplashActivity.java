f8adc7019f8db9d26ff4a785d6d11859a1d1bc64
NarendraUnnava <89415190+NarendraUnnava@users.noreply.github.com>
2022-03-29 09:57:31 -0500

    Update SplashActivity.java
    Narendra Unnava Commit

--- app/src/main/java/com/example/eshopping/SplashActivity.java
+++ app/src/main/java/com/example/eshopping/SplashActivity.java
@@ -13,9 +13,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         splash();
-
     }
-
     private void splash() {
         final boolean _active = true;
         final int _splashTime = 1000;

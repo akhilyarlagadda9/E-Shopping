289b9da9c914ec8be1961b02d76ee10d884b862b
rikeshg <89558538+rikeshg@users.noreply.github.com>
2022-04-25 10:22:52 -0500

    updated

--- app/src/main/java/com/example/eshopping/LoginActivity.java
+++ app/src/main/java/com/example/eshopping/LoginActivity.java
@@ -15,7 +15,6 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         bt_submit = (TextView) findViewById(R.id.bt_submit);

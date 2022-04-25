8f8aadc52a0a006f9524a05165c9787bb56565f1
rikeshg <89558538+rikeshg@users.noreply.github.com>
2022-04-25 10:18:34 -0500

    updated

--- app/src/main/java/com/example/eshopping/HomeActivity.java
+++ app/src/main/java/com/example/eshopping/HomeActivity.java
@@ -20,7 +20,6 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mContext=this;

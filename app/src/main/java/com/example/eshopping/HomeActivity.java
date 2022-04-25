07e882adca50a491f1c4854a54f86e4730f575dc
rikeshg <89558538+rikeshg@users.noreply.github.com>
2022-04-25 10:16:13 -0500

    updated

--- app/src/main/java/com/example/eshopping/HomeActivity.java
+++ app/src/main/java/com/example/eshopping/HomeActivity.java
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class HomeActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     HomeAdapter homeAdapter;

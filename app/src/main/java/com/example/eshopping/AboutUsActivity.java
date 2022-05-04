79c871cf125fe0c1fed1b0526b7cf368c21edb0a
akhilyarlagadda <S545513@nwmissouri.edu>
2022-05-04 13:46:38 -0500

    updated About US

--- app/src/main/java/com/example/eshopping/AboutUsActivity.java
+++ app/src/main/java/com/example/eshopping/AboutUsActivity.java
@@ -10,6 +10,6 @@ public class AboutUsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_details);
+        setContentView(R.layout.activity_about);
     }
 }

5bdcb146acb63892a242d18a1006f94b6225914b
akhilyarlagadda <S545513@nwmissouri.edu>
2022-05-04 11:26:17 -0500

    Create NavDto.java
    add nav

---
+++ app/src/main/java/com/example/eshopping/dtos/NavDto.java
@@ -0,0 +1,27 @@
+package com.example.eshopping.dtos;
+
+public class NavDto {
+    String title;
+    int img;
+
+    public NavDto(String title, int img) {
+        this.title = title;
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}

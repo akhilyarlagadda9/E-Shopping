e15b9f80022e33eddb86b5fbef76e68e19712dac
NarendraUnnava <89415190+NarendraUnnava@users.noreply.github.com>
2022-05-23 17:32:22 -0500

    Update AboutUsActivity.java

--- app/src/main/java/com/example/eshopping/AboutUsActivity.java
+++ app/src/main/java/com/example/eshopping/AboutUsActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class AboutUsActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
